284d3f98fd9aaf78041c32c67da66913c9be7daa
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-07-26 14:03:41 +0300

    ну и вот почему оно не работает то блять

--- aaa.py
+++ aaa.py
@@ -10,7 +10,7 @@ model = AutoGPTQForCausalLM.from_quantized(model_name, model_basename=model_base
 def get_answer(question, context):
     prompt = f"Вопрос: {question} Контекст: {context} Ответ:"
     encoded_input = tokenizer(prompt, return_tensors='pt', padding=True).to('cuda:0')
-    output = model.generate(**encoded_input, num_beams=1, max_new_tokens=160, no_repeat_ngram_size=2) ##изначально 4, 160, 2
+    output = model.generate(**encoded_input, num_beams=4, max_new_tokens=50, no_repeat_ngram_size=2) ##изначально 4, 160, 2
     answer = tokenizer.decode(output[0], skip_special_tokens=True).replace(prompt, "").strip()
     return answer
 
