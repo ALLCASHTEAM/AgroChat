3ec4a5ed188cf5987f8d946e03f27329e57800bb
Zomane <tsybulkin.step@mail.ru>
2023-07-27 11:20:48 +0300

    (

--- static/js/chat_bot.js
+++ static/js/chat_bot.js
@@ -1,24 +1,13 @@
 var sendForm = document.querySelector('#chatform'),
     textInput = document.querySelector('.chatbox'),
     chatList = document.querySelector('.chatlist'),
-    userBubble = document.querySelectorAll('.userInput'),
-    botBubble = document.querySelectorAll('.bot__output'),
-    animateBotBubble = document.querySelectorAll('.bot__input--animation'),
-    overview = document.querySelector('.chatbot__overview'),
-    hasCorrectInput,
-    imgLoader = false,
     animationCounter = 1,
-    animationBubbleDelay = 600,
-    input,
-    previousInput,
-    isReaction = false,
-    unkwnCommReaction = "I didn't quite get that.",
-    chatbotButton = document.querySelector(".submit-button")
-
+    animationBubbleDelay = 600;
 
 function scrollToBottom() {
   chatList.scrollTop = chatList.scrollHeight;
 }
+
 sendForm.onkeydown = function(e){
   if(e.keyCode == 13){
     e.preventDefault();
@@ -28,7 +17,7 @@ sendForm.onkeydown = function(e){
 
     //Empty textarea fix
     if(input.length > 0) {
-      createBubble(input)
+      createBubble(input);
     }
   }
 };
@@ -44,7 +33,7 @@ sendForm.addEventListener('submit', function(e) {
   if(input.length > 0) {
     createBubble(input);
   }
-}); //end of eventlistener
+});
 
 var createBubble = function(input) {
   //create input bubble
@@ -55,146 +44,16 @@ var createBubble = function(input) {
   chatBubble.innerHTML = input;
 
   //adds chatBubble to chatlist
-
-  scrollToBottom();
   chatList.appendChild(chatBubble);
-  checkInput(input);
   scrollToBottom();
+  clearInput();
 }
 
-
-// Эту функцию надо будет писать с нуля и я хз как ибо в js я не ебу
-var checkInput = function(input) {
-  hasCorrectInput = false;
-  isReaction = false;
-  //Checks all text values in possibleInput
-  for(var textVal in possibleInput){
-    //If user reacts with "yes" and the previous input was in textVal
-    if(input == "yes" || input.indexOf("yes") >= 0){
-      if(previousInput == textVal) {
-        console.log("sausigheid");
-
-        isReaction = true;
-        hasCorrectInput = true;
-        botResponse(textVal);
-      }
-    }
-    if(input == "no" && previousInput == textVal){
-      unkwnCommReaction = "For a list of commands type: Commands";
-      unknownCommand("I'm sorry to hear that :(")
-      unknownCommand(unkwnCommReaction);
-      hasCorrectInput = true;
-    }
-    //Is a word of the input also in possibleInput object?
-    if(input == textVal || input.indexOf(textVal) >=0 && isReaction == false){
-			console.log("succes");
-      hasCorrectInput = true;
-      botResponse(textVal);
-		}
-	}
-  //When input is not in possibleInput
-  if(hasCorrectInput == false){
-    console.log("failed");
-    unknownCommand(unkwnCommReaction);
-    hasCorrectInput = true;
-  }
-}
-
-// debugger;
-//и эту скорее всего с нуля, но я так же не ебу как
-function botResponse(textVal) {
-  //sets previous input to that what was called
-  // previousInput = input;
-
-  //create response bubble
-  var userBubble = document.createElement('li');
-  userBubble.classList.add('bot__output');
-
-
-
-    //Is the command a function?
-    if (typeof possibleInput[textVal] === "function") {
-      // console.log(possibleInput[textVal] +" is a function");
-    //adds input of textarea to chatbubble list item
-      userBubble.innerHTML = possibleInput[textVal]();
-    } else {
-      userBubble.innerHTML = possibleInput[textVal];
-    }
-
-  //add list item to chatlist
-  chatList.appendChild(userBubble) //adds chatBubble to chatlist
-
-  // reset text area input
+function clearInput() {
+  // Clear the text from the input field after sending
   textInput.value = "";
-}
-
-function unknownCommand(unkwnCommReaction) {
-  // animationCounter = 1;
-
-  //create response bubble
-  var failedResponse = document.createElement('li');
-
-  failedResponse.classList.add('bot__output');
-  failedResponse.classList.add('bot__output--failed');
-
-  //Add text to failedResponse
-  failedResponse.innerHTML = unkwnCommReaction; //adds input of textarea to chatbubble list item
-
-  //add list item to chatlist
-  chatList.appendChild(failedResponse) //adds chatBubble to chatlist
-
-  animateBotOutput();
-
-  // reset text area input
-  textInput.value = "";
-
-  //Sets chatlist scroll to bottom
-  chatList.scrollTop = chatList.scrollHeight;
-
-  animationCounter = 1;
-}
-
-function responseText(e) {
-
-  var response = document.createElement('li');
-
-  response.classList.add('bot__output');
-
-  //Adds whatever is given to responseText() to response bubble
-  response.innerHTML = e;
-
-  chatList.appendChild(response);
-
-  animateBotOutput();
-
-  console.log(response.clientHeight);
-
-  //Sets chatlist scroll to bottom
-  setTimeout(function(){
-    chatList.scrollTop = chatList.scrollHeight;
-    console.log(response.clientHeight);
-  }, 0)
-}
-
-function responseImg(e) {
-  var image = new Image();
-
-  image.classList.add('bot__output');
-  //Custom class for styling
-  image.classList.add('bot__outputImage');
-  //Gets the image
-  image.src = "/images/"+e;
-  chatList.appendChild(image);
-
-  animateBotOutput()
-  if(image.completed) {
-    chatList.scrollTop = chatList.scrollTop + image.scrollHeight;
-  }
-  else {
-    image.addEventListener('load', function(){
-      chatList.scrollTop = chatList.scrollTop + image.scrollHeight;
-    })
-  }
+  // Focus back on the input field
+  textInput.focus();
 }
 
 //change to SCSS loop
@@ -203,10 +62,3 @@ function animateBotOutput() {
   animationCounter++;
   chatList.lastElementChild.style.animationPlayState = "running";
 }
-
-function commandReset(e){
-  animationCounter = 1;
-  previousInput = Object.keys(possibleInput)[e];
-}
-
-
