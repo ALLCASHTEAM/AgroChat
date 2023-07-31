a0c31ea79d729a83666c90053ef278572edf454e
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-07-31 12:01:04 +0300

    q

--- chooseProd.py
+++ chooseProd.py
@@ -16,7 +16,7 @@ def IdentProd(text):
     encoded_input = tokenizer(promt, return_tensors='pt', add_special_tokens=False).to('cuda:0')
     output = model.generate(
         **encoded_input,
-        num_beams=2,
+        num_beams=1,
         do_sample=True,
         max_new_tokens=60
         )
