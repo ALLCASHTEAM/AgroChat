93d9e96ff69f344ace08535b17bcbbd15470fda4
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-07-31 14:05:36 +0300

    Update chooseProd.py

--- chooseProd.py
+++ chooseProd.py
@@ -1,14 +1,13 @@
 import os
 os.environ.setdefault('TF_ENABLE_ONEDNN_OPTS', '0')
-import tensorflow as tf
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 
-
-tokenizer = GPT2Tokenizer.from_pretrained("ai-forever/ruGPT-3.5-13B")
 model = GPT2LMHeadModel.from_pretrained("ai-forever/ruGPT-3.5-13B")
+tokenizer = GPT2Tokenizer.from_pretrained("ai-forever/ruGPT-3.5-13B")
 model.half()
-model.to('cuda:0')
+model.to('cuda')
+
 
 def IdentProd(text):
     file_path = './boosters.txt'
@@ -16,18 +15,16 @@ def IdentProd(text):
     with open(file_path, 'r') as file:
         content = file.read()
 
-    promt = "Представим что ты агроном-продавецконсультант, вот так выглядит твой каталог: "+content+"а покупатель спрашивает это: "+text+" "#cюда еще добавлять диалог этот, например из 20 сообщений предидущих и тогда заебися будет пахнуть наша пися
-    encoded_input = tokenizer(promt, return_tensors='pt', add_special_tokens=False).to('cuda:0')
+    promt = "Представим что ты агроном-продавецконсультант, вот так выглядит твой каталог: " + content + "а покупатель спрашивает это: " + text + " "  # cюда еще добавлять диалог этот, например из 20 сообщений предидущих и тогда заебися будет пахнуть наша пися
+    encoded_input = tokenizer(promt, return_tensors='pt', add_special_tokens=False).to('cuda')
     output = model.generate(
         **encoded_input,
         num_beams=2,
         do_sample=True,
         max_new_tokens=60
-        )
+    )
 
     print(tokenizer.decode(output[0], skip_special_tokens=True))
 
+
 IdentProd("Чем мне удобрить кукурузу")
-
-
-
