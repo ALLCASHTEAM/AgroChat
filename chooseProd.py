48e74fd003e194e9ea68772ae4b22e5b4f378849
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-07-31 12:57:59 +0300

    Update chooseProd.py

--- chooseProd.py
+++ chooseProd.py
@@ -4,7 +4,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 tokenizer = GPT2Tokenizer.from_pretrained("ai-forever/ruGPT-3.5-13B")
 model = GPT2LMHeadModel.from_pretrained("ai-forever/ruGPT-3.5-13B")
 model.half()
-model.to('cuda:0')
+model.to('cuda')
 
 def IdentProd(text):
     file_path = './boosters.txt'
@@ -13,7 +13,7 @@ def IdentProd(text):
         content = file.read()
 
     promt = "Представим что ты агроном-продавецконсультант, вот так выглядит твой каталог: "+content+"а покупатель спрашивает это: "+text+" "#cюда еще добавлять диалог этот, например из 20 сообщений предидущих и тогда заебися будет пахнуть наша пися
-    encoded_input = tokenizer(promt, return_tensors='pt', add_special_tokens=False).to('cuda:0')
+    encoded_input = tokenizer(promt, return_tensors='pt', add_special_tokens=False).to('cuda')
     output = model.generate(
         **encoded_input,
         num_beams=2,
