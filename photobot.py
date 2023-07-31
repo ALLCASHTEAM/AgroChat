5c6d94b6d632c2382edccef66191b626a2399e36
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-07-31 13:02:40 +0300

    SomeKopro

---
+++ photobot.py
@@ -0,0 +1,35 @@
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
+
+model_name = 'fffrrt/ruGPT-3.5-13B-GPTQ'
+model_basename = 'fffrrt/ruGPT-3.5-13B-GPTQ'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+model = AutoGPTQForCausalLM.from_quantized(model_name,
+                                           model_basename=model_basename,
+                                           use_safetensors=True,
+                                           trust_remote_code=True,
+                                           device="cuda",
+                                           use_triton=False,
+                                           quantize_config=None)
+
+file_path = './boosters.txt'
+with open(file_path, 'r') as file:
+        content = file.read()
+
+def IdentProd(text):
+
+    print(content)
+
+    promt = "Представим что ты агроном-продавецконсультант, вот так выглядит твой каталог: "+content+"а покупатель спрашивает это: "+text+" "#cюда еще добавлять диалог этот, например из 20 сообщений предидущих и тогда заебися будет пахнуть наша пися
+    encoded_input = tokenizer(promt, return_tensors='pt', max_length=8096).to('cuda')
+    output = model.generate(
+            **encoded_input,
+            num_beams=4,
+            no_repeat_ngram_size=2,
+            # num_return_sequences=5,
+            # do_sample=True
+            max_length=8096
+        )
+
+    print(tokenizer.decode(output[0], skip_special_tokens=True))
