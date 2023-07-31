8cd5cd1b34d1540f3263a32143f78f94149f742b
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-07-31 09:08:36 +0300

    Update chooseProd.py

--- chooseProd.py
+++ chooseProd.py
@@ -20,6 +20,8 @@ def IdentProd(text):
     with open(file_path, 'r') as file:
         content = file.read()
 
+    print(content)
+
     promt = "Представим что ты агроном-продавецконсультант, вот так выглядит твой каталог: "+content+"а покупатель спрашивает это: "+text+" "#cюда еще добавлять диалог этот, например из 20 сообщений предидущих и тогда заебися будет пахнуть наша пися
     encoded_input = tokenizer(promt, return_tensors='pt').to('cuda:0')
     output = model.generate(
