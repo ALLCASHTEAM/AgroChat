abc0f998fbbc46133755d89a5bdf93089bf9edb0
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-07-31 11:55:34 +0300

    Update chooseProd.py

--- chooseProd.py
+++ chooseProd.py
@@ -13,7 +13,7 @@ def IdentProd(text):
         content = file.read()
 
     promt = "Представим что ты агроном-продавецконсультант, вот так выглядит твой каталог: "+content+"а покупатель спрашивает это: "+text+" "#cюда еще добавлять диалог этот, например из 20 сообщений предидущих и тогда заебися будет пахнуть наша пися
-    encoded_input = tokenizer(promt, return_tensors='pt').to('cuda:0')
+    encoded_input = tokenizer(promt, return_tensors='pt', add_special_tokens=False).to('cuda:0')
     output = model.generate(
         **encoded_input,
         num_beams=2,
