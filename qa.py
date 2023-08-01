6f2693534528c0c46f00917d02174538d81e5b00
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-08-01 13:42:50 +0300

    Update qa.py

--- qa.py
+++ qa.py
@@ -1,23 +1,42 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline, TrainingArguments, Trainer
-from datasets import load_dataset
-from txtai.pipeline import HFTrainer
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, AutoConfig, Trainer, TrainingArguments
+from transformers.data.data_collator import DataCollatorWithPadding
+import torch
 
-ds = load_dataset("xtreme", "XQuAD.ru")
+# Загрузите свои данные для дообучения здесь
+# Вам нужно представить данные в формате, который подходит для модели вопрос-ответ
+# Пожалуйста, замените 'train_dataset' на свои данные
+train_dataset = ...
 
-
-
-# Load model directly
-# Use a pipeline as a high-level helper
 model_name_ru = "timpal0l/mdeberta-v3-base-squad2"
-trainer = HFTrainer()
 
-model = AutoModelForQuestionAnswering.from_pretrained(model_name_ru)
+# Загрузите токенизатор и конфигурацию модели
+tokenizer = AutoTokenizer.from_pretrained(model_name_ru)
+config = AutoConfig.from_pretrained(model_name_ru)
+model = AutoModelForQuestionAnswering.from_pretrained(model_name_ru, config=config)
 
-model, tokenizer = trainer(model, ds, task="question-answering", num_train_epochs=10)
+# Определите функцию для обработки данных и конфигурацию обучения
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+training_args = TrainingArguments(
+    output_dir="./output_dir",  # Укажите путь к сохранению модели и результатов
+    num_train_epochs=1,          # Количество эпох для обучения (можете увеличить для лучших результатов)
+    per_device_train_batch_size=8,
+    save_steps=1000,             # Сохранять модель каждые 1000 шагов обучения
+    save_total_limit=2,          # Ограничьте количество сохраненных моделей
+    evaluation_strategy="steps", # Оценивать результаты каждые 1000 шагов обучения
+    eval_steps=1000,             # Частота оценивания
+)
 
-from transformers import pipeline
+# Создайте объект Trainer для обучения модели
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+    tokenizer=tokenizer,
+)
 
-questions = pipeline("question-answering", model=model, tokenizer=tokenizer)
-questions("Чем удобрять кукурузу?", """Биостим Зерновой - жидкое аминокислотное удобрение-биостимулятор для листовых подкормок зерновых культур;
-Биостим Кукуруза - жидкое аминокислотное удобрение-биостимулятор для листовых подкормок кукурузы;
-Биостим Масличный - жидкое аминокислотное удобрение-биостимулятор для листовых подкормок масличных и бобовых культур;""")
+# Запустите обучение
+trainer.train()
+
+# Сохраните дообученную модель
+trainer.save_model("./fine_tuned_model")
