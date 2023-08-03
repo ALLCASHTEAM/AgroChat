6fb72fb8d45028fa78947c1b12162e21c3c1184e
pure1water <nvodanov3@gmail.com>
2023-08-03 14:24:08 +0300

    GOMO

--- database_handler.py
+++ database_handler.py
@@ -1,13 +1,18 @@
 import sqlite3
 
-def write_db(table, question, answer):
+def write_db_txt( question, answer, marked):
     con = sqlite3.connect("database/feedback.db")
     cur = con.cursor()
-    cur.execute(f"SELECT id FROM {table} WHERE id=(SELECT max(id) FROM {table});")
+
+    if marked:
+        cur.execute(f"SELECT id FROM marked_txt WHERE id=(SELECT max(id) FROM marked_txt);")
     id = int(cur.fetchone()[0]) + 1
-    cur.execute(f"INSERT INTO {table}(id, question, answer) VALUES({id}, '{question}', '{answer}');")
+    cur.execute(f"INSERT INTO marked_txt(id, question, answer) VALUES({id}, '{question}', '{answer}');")
     con.commit()
     con.close()
+
+def write_db_img():
+    pass
 
 
 def read_db(table, where_statement=None):
@@ -23,5 +28,5 @@ def read_db(table, where_statement=None):
     con.close()
 
 if __name__ == "__main__":
-    write_db('marked_txt', 'Гомо', 'dsa')
+    write_db_txt('marked_txt', 'Гомо', 'dsa')
     read_db("marked_txt", where_statement="id = 2")
