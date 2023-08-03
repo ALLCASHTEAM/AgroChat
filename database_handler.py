8b44d866d3b013971339b631b356a68e977509c2
pure1water <nvodanov3@gmail.com>
2023-08-03 13:39:37 +0300

    dbdbdb

---
+++ database_handler.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def write_db(table, question, answer):
+    con = sqlite3.connect("database/feedback.db")
+    cur = con.cursor()
+    cur.execute(f"SELECT id FROM {table} WHERE id=(SELECT max(id) FROM {table});")
+    id = int(cur.fetchone()[0]) + 1
+    cur.execute(f"INSERT INTO {table}(id, question, answer) VALUES({id}, '{question}', '{answer}');")
+    con.commit()
+    con.close()
+
+
+def read_db(table, where_statement=None):
+    con = sqlite3.connect("database/feedback.db")
+    cur = con.cursor()
+    if where_statement is not None:
+        cur.execute(f"SELECT * FROM {table} WHERE {where_statement}")
+    else:
+        cur.execute(f"SELECT * FROM {table}")
+
+    result = cur.fetchall()
+    print(result)
+    con.close()
+
+write_db('marked_txt', 'Гомо', 'dsa')
+read_db("marked_txt")
