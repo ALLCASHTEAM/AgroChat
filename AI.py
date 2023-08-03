73b44aa26ae93b864cdd35d45207070bb1e91a2a
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-08-03 13:53:42 +0300

    цйв

--- AI.py
+++ AI.py
@@ -1,59 +1,16 @@
-'''
-Модель берты которая по контексту должна отвечать
-но так как контекст очень хуевый, нам нечего делать кроме как обосраться
-'''
-import torch
-import numpy as np
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
 
-# Используем токенизатор и модель
-tokenizer = AutoTokenizer.from_pretrained("timpal0l/mdeberta-v3-base-squad2")
-model = AutoModelForQuestionAnswering.from_pretrained("timpal0l/mdeberta-v3-base-squad2")
+model = AutoGPTQForCausalLM.from_pretrained('Gaivoronsky/ruGPT-3.5-13B-8bit', device="cuda:0", use_triton=False)
+tokenizer = AutoTokenizer.from_pretrained('Gaivoronsky/ruGPT-3.5-13B-8bit')
 
-# Задаем текст с товарами, разделенными символом ';'
-file_path = './boosters.txt'
-
-with open(file_path, 'r', encoding='utf-8') as file:
-    content = file.read()
-# Ввод вопроса пользователем
-question = "Чем мне удобрить кукурузу?"
-
-# Токенизируем вопрос и текст товаров
-tokenized = tokenizer.encode_plus(question, content, add_special_tokens=False, return_tensors="pt")
-
-# Получаем токены
-tokens = tokenizer.convert_ids_to_tokens(tokenized['input_ids'][0])
-
-# Запускаем модель для получения ответа
-outputs = model(**tokenized)
-
-# Получаем позиции начала и конца ответа в токенах
-# Получаем позиции начала и конца ответа в токенах
-start_index = torch.argmax(outputs.start_logits)
-end_index = torch.argmax(outputs.end_logits)
-
-# Получаем текст ответа из токенов
-answer = tokenizer.decode(tokenized['input_ids'][0][start_index:end_index + 1], skip_special_tokens=True)
-
-# Разделяем текст на строки товаров
-items = content.split(";")
-
-# Функция для поиска ответа в строке товара
-def find_answer_in_item(item, question):
-    if question.lower() in item.lower():
-        return item.strip()
-    return None
-
-# Ищем ответ среди строк товаров
-found_answer = None
-for item in items:
-    found_answer = find_answer_in_item(item, question)
-    if found_answer:
-        break
-
-# Выводим результат
-if found_answer:
-    print('Вопрос:', question)
-    print('Ответ:', found_answer)
-else:
-    print('Ответ не найден.')
+request = "Человек: Сколько весит жираф? Помощник: "
+encoded_input = tokenizer(request, return_tensors='pt', \
+                          add_special_tokens=False).to('cuda')
+output = model.generate(
+    **encoded_input,
+    num_beams=2,
+    do_sample=True,
+    max_new_tokens=100
+)
+print(tokenizer.decode(output[0], skip_special_tokens=True))
--- qa.py
+++ qa.py
@@ -1,42 +1,28 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, AutoConfig, Trainer, TrainingArguments
-from transformers.data.data_collator import DataCollatorWithPadding
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import torch
+model_name = "ai-forever/FRED-T5-1.7B"  # Используем базовую модель T5, вы можете заменить ее на любую другую модель T5
 
-# Загрузите свои данные для дообучения здесь
-# Вам нужно представить данные в формате, который подходит для модели вопрос-ответ
-# Пожалуйста, замените 'train_dataset' на свои данные
-train_dataset = ...
+# Загрузка модели и токенизатора
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name,eos_token='</s>')
 
-model_name_ru = "timpal0l/mdeberta-v3-base-squad2"
+with open('boosters.txt', 'r', encoding='utf-8') as f:
+    context = f.read().replace(',', '').replace(';', '. ').replace('..', '.')
+# Задайте вопрос и контекст
+question = "Чем мне удобрять кукурузу?"
 
-# Загрузите токенизатор и конфигурацию модели
-tokenizer = AutoTokenizer.from_pretrained(model_name_ru)
-config = AutoConfig.from_pretrained(model_name_ru)
-model = AutoModelForQuestionAnswering.from_pretrained(model_name_ru, config=config)
+print(context)
+# T5 ожидает, что задача будет указана во входном тексте, в формате "task: input"
+input_text = f"вопрос: {question}  контекст: {context}"
 
-# Определите функцию для обработки данных и конфигурацию обучения
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-training_args = TrainingArguments(
-    output_dir="./output_dir",  # Укажите путь к сохранению модели и результатов
-    num_train_epochs=1,          # Количество эпох для обучения (можете увеличить для лучших результатов)
-    per_device_train_batch_size=8,
-    save_steps=1000,             # Сохранять модель каждые 1000 шагов обучения
-    save_total_limit=2,          # Ограничьте количество сохраненных моделей
-    evaluation_strategy="steps", # Оценивать результаты каждые 1000 шагов обучения
-    eval_steps=1000,             # Частота оценивания
-)
+# Токенизируем входной текст
+inputs = tokenizer.encode(input_text, return_tensors='pt')
 
-# Создайте объект Trainer для обучения модели
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=train_dataset,
-    tokenizer=tokenizer,
-)
+# Получаем ответ от модели
+input_ids=torch.tensor([tokenizer.encode(input_text)])
+outputs = model.generate(input_ids, eos_token_id=tokenizer.eos_token_id, early_stopping=True, max_length=4096)
 
-# Запустите обучение
-trainer.train()
+# Декодируем ответ
+answer = tokenizer.decode(outputs[0][1:])
 
-# Сохраните дообученную модель
-trainer.save_model("./fine_tuned_model")
+print(answer)
