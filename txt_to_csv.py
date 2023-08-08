797bae0d9c042777e14b794153e60a3a721ee08d
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-08-08 15:43:55 +0300

    Update txt_to_csv.py

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 def convert(filename):
+    mass = []
     with open(filename, 'r', encoding='utf-8') as f:
         c = f.read().count('\n')
     with open(filename, 'r', encoding='utf-8') as k:
         for i in range(c):
 
             data = k.readline().replace('\n', ' ').split(' - ')
-            print(data)
+            mass.append(data)
 
+    mass = pd.DataFrame(mass).to_csv('qaByGPTWithOutDots.csv')
 
 if __name__ == '__main__':
     convert('qaByGPTWithOutDots.txt')
