ee4f679b8731720460714df9de5b01b8760e21dc
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-08-08 09:47:42 +0300

    хуй вам в уши

--- alias.py
+++ alias.py
@@ -1,15 +1,43 @@
-def remove_commas_and_periods(file_path, output_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        text = file.read()
+def bart_parse():
+    import time
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    import undetected_chromedriver as uc
+    driver = uc.Chrome()
 
-    # Удаление запятых и точек
-    text_without_commas_and_periods = text.replace(',', '').replace('.', '')
+    u_name = 'grisasafronov219@gmail.com'
+    pass_ = 'Gs20042912'
 
-    with open(output_path, 'w', encoding='utf-8') as output_file:
-        output_file.write(text_without_commas_and_periods)
+    driver.get("https://bard.google.com")
 
-# Пример использования
-input_file_path = './qaByGPTWithOutDots.txt'   # Путь к исходному файлу
-output_file_path = './qaByGPTWithOutDots.txt' # Путь к файлу, в котором будут результаты без запятых и точек
+    # login
+    driver.find_element(By.XPATH, '/html/body/chat-app/side-navigation/mat-sidenav-container/mat-sidenav-content/main/welcome-window/div/div/div/div[2]/button').click()
+    WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]'))).send_keys(u_name)
+    driver.find_element(By.XPATH, '//*[@id="identifierNext"]').click()
+    time.sleep(5)
+    WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input'))).send_keys(pass_)
+    driver.find_element(By.XPATH, '//*[@id="passwordNext"]').click()
 
-remove_commas_and_periods(input_file_path, output_file_path)
+    # bart queries
+    cnt = 0
+
+    with open('rofls/test.txt', mode='r', encoding='utf-8') as f:
+        data = f.readlines()
+
+    while cnt <= len(data):
+        query = str(f'''вот ссылка на товар: https://betaren.ru/catalog/sredstva-zashchity-rasteniy/gerbitsidy/acetal_pro/
+Мне нужны 30 пар вопрос-ответ такого формата: "Вопрос: Каков химический класс "Азорро, КС"?
+Ответ: "Азорро, КС" относится к химическим классам бензимидазолов и стробилуринов."
+важно что информация для составления вопросов должна браться только с этой ссылки и больше ни с какого источника''')
+        WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mat-input-0"]'))).send_keys(query)
+        driver.execute_script("arguments[0].click();", WebDriverWait(driver, 100000).until(EC.element_to_be_clickable((By.XPATH, '/html/body/chat-app/side-navigation/mat-sidenav-container/mat-sidenav-content/main/chat-window/div[1]/div[2]/input-area/div/div/button'))))
+        resp = WebDriverWait(driver, 1000000).until(EC.presence_of_element_located((By.XPATH, f'/html/body/chat-app/side-navigation/mat-sidenav-container/mat-sidenav-content/main/chat-window/div[1]/div[1]/infinite-scroller/div[{cnt+1}]/model-response/div/response-container/div'))).get_attribute('innerHTML')
+
+        with open(f'rofls/test/{str(cnt)}.txt', mode='w+', encoding='utf-8') as f:
+            f.write(resp + '\n' + data[cnt])
+
+        cnt += 1
+
+if True:
+    bart_parse()
