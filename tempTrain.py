67e57c5e46c2ca63b8448784962fb0ddcf009f60
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-08-08 13:50:00 +0300

    anotherTry

--- tempTrain.py
+++ tempTrain.py
@@ -1,13 +1,10 @@
 from deeppavlov import train_model
 
-# Путь к вашему текстовому файлу
-data_path = 'qaByGPTWithOutDots.txt'
-
 # Путь к JSON-файлу с конфигурациями
-config_path = 'cfg.json'
+config_path = 'path/to/cfg.json'
 
 # Загрузка и обучение модели с использованием конфигураций из JSON-файла
-model = train_model(config_path, data_path=data_path)
+model = train_model(config_path)
 
 # Обучение с выводом логов каждые 0.1 эпохи
 for epoch in range(1, model.config['train']['epochs'] + 1):
