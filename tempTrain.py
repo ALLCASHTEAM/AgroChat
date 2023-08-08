b3b314b8ad2757f5559e04eb8f0426333596fdad
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-08-08 14:11:03 +0300

    Update tempTrain.py

--- tempTrain.py
+++ tempTrain.py
@@ -1,8 +1,8 @@
 import torch
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizer, AutoTokenizer, TrainingArguments, Trainer, default_data_collator
 
 # Загружаем токенизатор
-tokenizer = BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased')
+tokenizer = AutoTokenizer.from_pretrained('DeepPavlov/rubert-base-cased')
 
 # Загружаем данные
 data = []
@@ -21,8 +21,34 @@ for text, label in data:
   attention_masks.append(encoded_inputs['attention_mask'])
   labels.append(label)
 
+print(data,text,label)
+
 # Создаем модель
 model = BertForSequenceClassification.from_pretrained('DeepPavlov/rubert-base-cased')
+
+training_args = TrainingArguments(
+    output_dir="/mnt/data/training",
+    overwrite_output_dir=True,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    gradient_accumulation_steps=16,
+    warmup_steps=100,
+    num_train_epochs=3,
+    logging_steps=10,
+
+
+)
+
+# Создаем тренер
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=data,
+    data_collator=default_data_collator,
+    optimizers = (torch.optim.AdamW(model.parameters(),lr=1e-5),None),
+
+)
+
 
 # Обучаем модель
 model.train(input_ids, attention_masks, labels, epochs=10)
