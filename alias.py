38085803168f3996a6bc926070dc7bce1472b58a
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-08-08 11:52:59 +0300

    trainer

--- alias.py
+++ alias.py
@@ -1,43 +1,61 @@
-def bart_parse():
-    import time
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    import undetected_chromedriver as uc
-    driver = uc.Chrome()
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, TrainingArguments, Trainer, default_data_collator
+from datasets import Dataset
+import torch
 
-    u_name = 'grisasafronov219@gmail.com'
-    pass_ = 'Gs20042912'
+model_name = "timpal0l/mdeberta-v3-base-squad2"
+data_file = "qaByGPTWithOutDots.txt"
 
-    driver.get("https://bard.google.com")
+# Загрузим модель и токенизатор
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # login
-    driver.find_element(By.XPATH, '/html/body/chat-app/side-navigation/mat-sidenav-container/mat-sidenav-content/main/welcome-window/div/div/div/div[2]/button').click()
-    WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]'))).send_keys(u_name)
-    driver.find_element(By.XPATH, '//*[@id="identifierNext"]').click()
-    time.sleep(5)
-    WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input'))).send_keys(pass_)
-    driver.find_element(By.XPATH, '//*[@id="passwordNext"]').click()
+# Загрузка данных из текстового файла
+with open(data_file, "r", encoding="utf-8") as f:
+    lines = f.readlines()
 
-    # bart queries
-    cnt = 0
+# Создание датасета
+qa_data = {"question": [], "context": []}
+for line in lines:
+    question, context = line.strip().split(" - ")
+    qa_data["question"].append(question)
+    qa_data["context"].append(context)
 
-    with open('rofls/test.txt', mode='r', encoding='utf-8') as f:
-        data = f.readlines()
+dataset = Dataset.from_dict(qa_data)
 
-    while cnt <= len(data):
-        query = str(f'''вот ссылка на товар: https://betaren.ru/catalog/sredstva-zashchity-rasteniy/gerbitsidy/acetal_pro/
-Мне нужны 30 пар вопрос-ответ такого формата: "Вопрос: Каков химический класс "Азорро, КС"?
-Ответ: "Азорро, КС" относится к химическим классам бензимидазолов и стробилуринов."
-важно что информация для составления вопросов должна браться только с этой ссылки и больше ни с какого источника''')
-        WebDriverWait(driver, 100000).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mat-input-0"]'))).send_keys(query)
-        driver.execute_script("arguments[0].click();", WebDriverWait(driver, 100000).until(EC.element_to_be_clickable((By.XPATH, '/html/body/chat-app/side-navigation/mat-sidenav-container/mat-sidenav-content/main/chat-window/div[1]/div[2]/input-area/div/div/button'))))
-        resp = WebDriverWait(driver, 1000000).until(EC.presence_of_element_located((By.XPATH, f'/html/body/chat-app/side-navigation/mat-sidenav-container/mat-sidenav-content/main/chat-window/div[1]/div[1]/infinite-scroller/div[{cnt+1}]/model-response/div/response-container/div'))).get_attribute('innerHTML')
+# Функция для предобработки данных
+def prepare_train_features(examples):
+    encodings = tokenizer(
+        examples["question"], examples["context"],
+        truncation=True, padding="max_length", max_length=383,
+        return_offsets_mapping=True
+    )
+    # Ваш код для обработки позиций начала и конца ответов
 
-        with open(f'rofls/test/{str(cnt)}.txt', mode='w+', encoding='utf-8') as f:
-            f.write(resp + '\n' + data[cnt])
+    return encodings
 
-        cnt += 1
+# Применяем предобработку данных
+tokenized_dataset = dataset.map(prepare_train_features, batched=True)
 
-if True:
-    bart_parse()
+# Создаем аргументы для обучения
+training_args = TrainingArguments(
+    output_dir="./training",
+    overwrite_output_dir=True,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    gradient_accumulation_steps=16,
+    warmup_steps=100,
+    num_train_epochs=3,
+    logging_steps=1000,
+)
+
+# Создаем тренер
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset,
+    data_collator=default_data_collator,
+    optimizers=(torch.optim.AdamW(model.parameters(), lr=1e-5), None),
+)
+
+# Запускаем обучение
+trainer.train()
