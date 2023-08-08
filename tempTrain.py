d5c6dffbb0459b397066f0addf5dc9f76a009954
UglyXd <80771392+UglyXd@users.noreply.github.com>
2023-08-08 12:49:25 +0300

    Create tempTrain.py

---
+++ tempTrain.py
@@ -0,0 +1,31 @@
+import torch
+from transformers import BertForSequenceClassification, BertTokenizer
+
+# Загружаем токенизатор
+tokenizer = BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased')
+
+# Загружаем данные
+data = []
+with open('qaByGPTWithOutDots.txt', 'r',encoding='utf-8') as f:
+  for line in f:
+    text, label = line.split(' - ')
+    data.append((text, label))
+
+# Преобразуем данные в формат, понятный модели
+input_ids = []
+attention_masks = []
+labels = []
+for text, label in data:
+  encoded_inputs = tokenizer(text, return_tensors='pt')
+  input_ids.append(encoded_inputs['input_ids'])
+  attention_masks.append(encoded_inputs['attention_mask'])
+  labels.append(label)
+
+# Создаем модель
+model = BertForSequenceClassification.from_pretrained('DeepPavlov/rubert-base-cased')
+
+# Обучаем модель
+model.fit(input_ids, attention_masks, labels, epochs=10)
+
+# Сохраняем модель
+model.save_pretrained('model')
