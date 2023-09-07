271d76acd8eaab6589096cde334230c2c96cf6b1
Zomane <tsybulkin.step@mail.ru>
2023-09-07 21:42:30 +0300

    целых 146 файлов в комите изизизизизи

--- replaser.py
+++ replaser.py
@@ -1,13 +1,28 @@
-file_path = 'C:/Users/User/Documents/GitHub/AgroChat/rofls/Азорро, КС.txt'  # Путь к вашему файлу
+import os
 
-with open(file_path, 'r', encoding='utf-8') as file:
-    lines = file.readlines()
+folder_path = './rofls'  # Путь к вашей папке
 
-# Применяем замены и обработку для каждой строки
-for i in range(len(lines)):
-    if 'Вопрос:' in lines[i] or 'Ответ:' in lines[i]:
-        lines[i] = lines[i].replace('Вопрос:', '{"question":"H:').replace('\nОтвет:', '\\nB: <extra_id_0>","answer":"<extra_id_0>').replace('"', '/"')
-    else:
-        lines.pop(i)
-with open(file_path, 'w', encoding='utf-8') as file:
-    file.writelines(lines)
+# Получаем список файлов в папке "rofls"
+files = os.listdir(folder_path)
+
+# Проходим по каждому файлу в папке
+for filename in files:
+    file_path = os.path.join(folder_path, filename)
+
+    # Проверяем, что это текстовый файл
+    if filename.endswith('.txt'):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        # Создаем новый список для измененных строк
+        new_lines = []
+
+        # Применяем замены и обработку для каждой строки
+        for line in lines:
+            if 'Вопрос:' in line or 'Ответ:' in line:
+                line = line.replace('"', '').replace('Эко Плюс ', '').replace('Эко плюс', '').replace('Новинка ', '').replace('Эко Плюс', '').replace('Новинка', '')
+            new_lines.append(line)
+
+        # Записываем измененные строки обратно в тот же файл
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.writelines(new_lines)
