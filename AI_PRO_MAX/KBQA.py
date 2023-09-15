13a1956ddcdc6997a3743ef691be27234afc8552
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-09-15 16:36:54 +0300

    Update KBQA.py

--- AI_PRO_MAX/KBQA.py
+++ AI_PRO_MAX/KBQA.py
@@ -20,8 +20,8 @@ def find_best_matches(user_query, sentence_model):
 
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-
-    similarities = util.pytorch_cos_sim(sentence_model.encode(user_query), sentence_model.encode(lines))[0]
+    lines = list(map(lambda x: x.split("|")[0], lines))
+    similarities = util.pytorch_cos_sim(sentence_model.encode(user_query.lower()), sentence_model.encode(list(map(lambda x: x.lower(), lines))))[0]
 
     # Создаем список совпадений и их оценок вместе с исходными индексами строк
     searcher = Search_script.Search(upgrade_kbqa.cosine_similarity1([user_query], ident_file.lower().split(' ')))
@@ -34,7 +34,7 @@ def find_best_matches(user_query, sentence_model):
 
 
 if __name__ == "__main__":
-    user_query = "Преимущество биостим кукуруза?"
+    user_query = "Преимущества биостим кукуруза?"
     sentence_model = initialize_sentence_model()
     matches = find_best_matches(user_query, sentence_model)
 
@@ -43,15 +43,15 @@ if __name__ == "__main__":
     print("\nscript: KBQA.py\n################################ ПОИСК ПО БАЗЕ ЗНАНИЙ #################################")
     print("Вопрос пользователя: ", user_query)
 
-    print("Подобран похожий вопрос: ", best_result.split('|')[0].strip().replace("('", ""), "\nПодобран ответ: ",
-          best_result.split('|')[1].strip().split('\\n')[0].strip(), "\nScore: ",
-          best_result.split("tensor(")[1].split(")")[0])
+    print("Подобран похожий вопрос: ", best_result.split(',')[0].strip().replace("('", ""), 'Score:',
+          # best_result.split('|')[1].strip().split('\\n')[0].strip(), "\nScore: ",
+          best_result.split(", tensor(")[1].split(")")[0])
 
     print("\nТоп 4 варианта по скор: ")
     for i in range(1, len(matches)):
         if i > 4:
             break
-        print("Топ ", i, ": ", str(matches[i]).split('|')[0].strip().replace("('", ""), "\nПодобран ответ: ",
-              str(matches[i]).split('|')[1].strip().split('\\n')[0].strip(), "\nScore: ",
-              str(matches[i]).split("tensor(")[1].split(")")[0])
+        # print("Топ ", i, ": ", str(matches[i]).split('|')[0].strip().replace("('", ""), "\nПодобран ответ: ",
+        #       str(matches[i]).split('|')[1].strip().split('\\n')[0].strip(), "\nScore: ",
+        #       str(matches[i]).split("tensor(")[1].split(")")[0])
     print("\n################################ КОНЕЦ ПОИСКА ПО БАЗЕ ЗНАНИЙ #################################")
