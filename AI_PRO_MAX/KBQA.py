2d87f5d570f13c683ceef75f81ebec2cdc384c5f
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-11-05 17:00:20 +0300

    Revert "kbqa2.0"
    This reverts commit 9938e0031ccf9f7a5308fad5f096065bf6bf6baf.

--- AI_PRO_MAX/KBQA.py
+++ AI_PRO_MAX/KBQA.py
@@ -11,7 +11,8 @@ def initialize_sentence_model():
 
 def find_best_matches(user_query, sentence_model):
     liness = realsweg.list_return(user_query)
-    lines = list(map(lambda x: x.split("|")[0], liness))
+    liness = list(map(lambda x: x.split("|"), liness))
+    lines = list(zip(*liness))[0]
     try:
         similarities = util.pytorch_cos_sim(sentence_model.encode(user_query.lower()),
                                             sentence_model.encode(list(map(lambda x: x.lower(), lines))))[0]
@@ -28,22 +29,21 @@ def find_best_matches(user_query, sentence_model):
 
 def KBQA_search(user_query):
     sentence_model = initialize_sentence_model()
-
     matches, liness = find_best_matches(user_query, sentence_model)
-    answer = ""
-    best_result = str(matches[0]).replace("('", "").split("?")[0]
-    for line in liness:
-        # Проверяем, содержит ли строка первую половину
-        if best_result in line:
-            # Если да, выводим эту строку
-            answer = line.split("|")[1]
-    print(answer)
+    best_result = list(map(lambda x: str(x).replace("('", "").split("?")[0], matches))
+    found_answers = ''
+    for question in best_result:
+        for q, a in liness:
+            if q.strip().lower().replace('?', '') == question.strip().lower():
+                found_answers += f',{a}'
+    found_answers = found_answers[1:]
+    print(found_answers)
     print("\nscript: KBQA.py\n################################ ПОИСК ПО БАЗЕ ЗНАНИЙ #################################")
     print("Вопрос пользователя: ", user_query)
     print("\n################################ КОНЕЦ ПОИСКА ПО БАЗЕ ЗНАНИЙ #################################")
-    return (answer)
+    return (found_answers)
 
 
 if __name__ == "__main__":
-    user_query = "Чем удобрить свеклу?"
+    user_query = "Чем обрабатывать сою?"
     KBQA_search(user_query)
--- AI_PRO_MAX/realsweg.py
+++ AI_PRO_MAX/realsweg.py
@@ -1,18 +1,18 @@
 import re
 import json
-from gensim.models import Word2Vec
+from gensim.models import Word2Vec, KeyedVectors
 from AI_PRO_MAX import hash_list
 
 # Путь к сохраненной модели
-model_path = './AI_PRO_MAX/word2vec_model.w2v'
-index_json_path = './AI_PRO_MAX/index.json'
+model_path = './model.bin'
+index_json_path = './index.json'
 # Загрузка модели
-w2v_model = Word2Vec.load(model_path)
+
 with open(index_json_path, 'r', encoding='utf-8') as f_json:
     loaded_index = json.load(f_json)
 rhash = hash_list.hash_creator()
 
-
+w2v_model = KeyedVectors.load_word2vec_format(model_path, binary=True)
 # Функция для поиска файлов и позиций вопросов по заданному вопросу
 
 
@@ -27,8 +27,8 @@ def search_with_w2v_synonyms(question, index, w2v_model, topn=5):
         # Добавляем слово в поиск
         search_terms = [word]
         # Если слово есть в модели, ищем его синонимы
-        if word in w2v_model.wv:
-            similar_words = w2v_model.wv.most_similar(word, topn=topn)
+        if word in w2v_model:
+            similar_words = w2v_model.most_similar(word, topn=topn)
             # Добавляем синонимы в поисковые термины
             search_terms.extend([similar[0] for similar in similar_words])
         # Для каждого поискового термина обновляем результаты
@@ -47,4 +47,5 @@ def list_return(sample_question: str) -> list:
 
 
 if __name__ == '__main__':
-    print(list_return("Чем мне удобрить свеклу?"))
+    for i in list_return("Чем мне удобрить свеклу?"):
+        print(i)
