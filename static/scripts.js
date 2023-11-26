0039ec8c9a07fd36f98ee8333fea5c128b63020c
Zomane <tsybulkin.step@mail.ru>
2023-11-26 23:51:23 +0300

    Тут много говна но я(с денди) все пофиксили слава мне

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,4 @@
-
-
 var waitingForResponse = false;
-
-
-
-
 
 document.getElementById('infoWrapper').addEventListener('click', function(){
 	let obj = document.getElementById('dialogueInfoWrapper');
@@ -28,52 +22,42 @@ document.getElementById('about').addEventListener('click', function(){
 });
 
 
-
-
-
 // ONLOAD AND EVENT TRIGGERS
 
   // handle onload actions
   window.addEventListener("load", (e) =>{
 
   // check if there's something in localStorage and load it into chat
-  	try{
-	    if (loadFromLocal("user").length > 0){
-	      userStory = loadFromLocal("user").split(";").slice(1);
-	      botStory = loadFromLocal("bot").split(";").slice(1);
+    if (loadFromLocal("user").length > 0){
+      userStory = loadFromLocal("user").split(";").slice(1);
+      botStory = loadFromLocal("bot").split(";").slice(1);
 
-	      for (let i = 0; i < userStory.length && i < botStory.length; i++){
-	        userData = userStory[i];
-	        botData = botStory[i];
+      for (let i = 0; i < userStory.length && i < botStory.length; i++){
+        userData = userStory[i];
+        botData = botStory[i];
 
-	        // check if there is an image
-	        if (userData.split("\\image").length == 2){
-	          userDataText = userData.split("\\")[0].split(":").slice(-1);
-	          userImageText = userData.split("\\")[1].split(":").slice(-1);
-	          makeUserBubble(userDataText, userImageText);
-	        }else{
-	          makeUserBubble(userData.split(":").slice(-1));
-	        }
-	        makeBotBubble(botData.split(":").slice(-1));
-	      }
-	      scrollToBottom();
-	    }}
-	 catch{
-
-	 }
+        //check if there is image
+        if (userData.split("\\image").length == 2){
+          userDataText = userData.split("\\")[0].split(":").slice(-1);
+          userImageText = userData.split("\\")[1].split(":").slice(-1);
+          makeUserBubble(userDataText, userImageText);
+        }else{
+          makeUserBubble(userData.split(":").slice(-1));
+        }
+        makeBotBubble(botData.split(":").slice(-1));
+      }
+      scrollToBottom();
+    }
 
   });
-  let fileInput = document.querySelector('.send_img');
-  let chatList = document.querySelector('.chatlist');
-  console.log("старт"+fileInput);
-      console.log("старт"+chatList);
+  const fileInput = document.querySelector('.send_img');
+  const chatList = document.querySelector('.chatlist');
   document.getElementsByClassName("send_img")[0].addEventListener("input", (()=>{
     console.log(document.getElementsByClassName("send_img")[0]);
     document.getElementsByClassName("attach-button")[0].textContent = fileInput.files[0].name;
   }));
 // MISC
-  // loader
-  var loader = '<svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve"> <circle fill="#fff" stroke="none" cx="6" cy="50" r="6"> <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.1"/> </circle> <circle fill="#fff" stroke="none" cx="26" cy="50" r="6"> <animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.2"/> </circle> <circle fill="#fff" stroke="none" cx="46" cy="50" r="6"> <animate attributeName="opacity"dur="1s" values="0;1;0" repeatCount="indefinite" begin="0.3"/></circle></svg>';
+
   // scroll to bottom
   function scrollToBottom() {
     var chatList = document.querySelector('#scroller');
@@ -100,21 +84,16 @@ document.getElementById('about').addEventListener('click', function(){
   });
 
   // handle image input
-  function loadImage(){
-  	try{
-	    if (document.getElementsByClassName("send_img")[0].files[0]){
-	      var imageObj = document.getElementsByClassName("send_img")[0];
-	      document.getElementsByClassName("attach-button").textContent = imageObj.name;
-	      return imageObj.files[0]
-	    }
-	    else{
-      		return null
-    	}
-	}catch{
-		return null
-	}
-
-  }
+  //  function loadImage(){
+  //    if (document.getElementsByClassName("send_img")[0].files[0]){
+  //      var imageObj = document.getElementsByClassName("send_img")[0];
+  //      document.getElementsByClassName("attach-button").textContent = imageObj.name;
+  //      return imageObj.files[0]
+  //    }
+  //    else{
+  //      return null
+  //    }
+  //  }
 
 // CHAT BUBBLES
 
@@ -135,7 +114,6 @@ document.getElementById('about').addEventListener('click', function(){
       container.appendChild(imageDiv);
     }
 
-
     // Append the text
     var textDiv = document.createElement('div');
     textDiv.textContent = text;
@@ -147,86 +125,27 @@ document.getElementById('about').addEventListener('click', function(){
     // Append the chat bubble to the chat list (assuming you have a chatList defined)
     chatList.appendChild(chatBubble);
 
+    scrollToBottom();
+
     return chatBubbleCounter + 1
   }
 
-  function replaceBotAnimwithBubble(responce_text){
-
-    // Находим существующий элемент li с классом "test-output"
-    var existingLi = document.querySelector('.test-output');
-
-    // Создаем новый элемент li
-    var botBubbleCounter = document.getElementsByClassName("bot__output").length + 1; // Увеличиваем счетчик на 1
-    var chatBubble_bot = document.createElement('li');
-    chatBubble_bot.classList.add('bot__output', 'bot__output--standard', 'animateBubble', 'id-' + botBubbleCounter);
-
-    // Вам нужно добавить текст в ваш элемент, вместо text
-    chatBubble_bot.innerHTML = responce_text;
-
-    // Заменяем существующий элемент на новый элемент
-    existingLi.parentNode.replaceChild(chatBubble_bot, existingLi);
-    //saveStateToLocalStorage();
-    saveHtmlToLocalStorage();
-
-  }
-// Save the current HTML code to local storage
-function saveHtmlToLocalStorage() {
-    var htmlCode = document.documentElement.outerHTML;
-    localStorage.setItem('savedHtmlCode', htmlCode);
-}
-
-// Restore the HTML code from local storage
-function restoreHtmlFromLocalStorage() {
-    var savedHtmlCode = localStorage.getItem('savedHtmlCode');
-    if (savedHtmlCode) {
-      console.log(fileInput);
-      console.log(chatList);
-        document.open();
-        document.write(savedHtmlCode);
-        document.close();
-        console.log(fileInput);
-      console.log(chatList);
-    }
-}
-
-// Save the HTML code when the page is unloaded (e.g., before a refresh or navigation)
-window.addEventListener('beforeunload', saveHtmlToLocalStorage);
-
-// Restore the HTML code when the page is loaded
-document.addEventListener('DOMContentLoaded', restoreHtmlFromLocalStorage);
-
   // make bot bubble with unique class
   function makeBotBubble(text, image=null){
-    //var botBubbleCounter = document.getElementsByClassName("bot__output").length;
-    var botBubbleCounter = document.getElementsByClassName("test-output").length;
-    //var chatBubble_bot = document.createElement('li');
-    //chatBubble_bot.classList.add('test-output','bot__output', 'bot__output--standard', 'animateBubble', "id-" + (botBubbleCounter + 1));
-    //chatBubble_bot.innerHTML = text;
-    //chatList.appendChild(chatBubble_bot);
-
-    var li = document.createElement('li');
-    li.classList.add("test-output", "id-"+(botBubbleCounter + 1));
-
-    // Создаем три элемента div с классом "dot" и стилем "--delay"
-    var dot1 = document.createElement('div');
-    dot1.className = 'dot';
-    dot1.style.setProperty('--delay', '200ms');
-
-    var dot2 = document.createElement('div');
-    dot2.className = 'dot';
-    dot2.style.setProperty('--delay', '300ms');
-
-    var dot3 = document.createElement('div');
-    dot3.className = 'dot';
-    dot3.style.setProperty('--delay', '400ms');
-
-    // Добавляем элементы div в элемент li
-    li.appendChild(dot1);
-    li.appendChild(dot2);
-    li.appendChild(dot3);
-
-    chatList.appendChild(li);
-
+    if (text.length == 0){
+      var botBubbleCounter = document.getElementsByClassName("bot__output").length;
+      var chatBubble_bot = document.createElement('li');
+      chatBubble_bot.classList.add('bot__output', 'bot__output--standard', 'animateBubble', "id-" + (botBubbleCounter + 1));
+      chatBubble_bot.innerHTML = '<span class="loader loader_custom_zalupa" ></span>';
+      chatList.appendChild(chatBubble_bot);
+    }else{
+      var botBubbleCounter = document.getElementsByClassName("bot__output").length;
+      var chatBubble_bot = document.createElement('li');
+      chatBubble_bot.classList.add('bot__output', 'bot__output--standard', 'animateBubble', "id-" + (botBubbleCounter + 1));
+      chatBubble_bot.innerHTML = text;
+      chatList.appendChild(chatBubble_bot);
+    }
+    scrollToBottom();
     return botBubbleCounter + 1
   }
 
@@ -266,28 +185,30 @@ document.addEventListener('DOMContentLoaded', restoreHtmlFromLocalStorage);
   function makeBubbles()
   {
     var text = document.querySelector('.chatbox').value;
-  	waitingForResponse = true;
-
+    console.log(text.length);
     // if text is smaller than 2 symbols and there is no image throw alert
-    if (text.trim() <= 2 && !loadImage()){
-      alert("Слишком короткое сообщение");
+    // if (text.trim() <= 2 && !loadImage()) !!!
+    if (text.trim().length <= 2){
+      alert("Слишком короткое сообщение!");
       return
     }
-    if (loadImage() && text.trim() > 0){
-      alert("текст и картинка не могут быть отправленны одновременно");
-      document.getElementsByClassName("send_img")[0].value = "";
-      document.getElementsByClassName("chatbox")[0].value = "";
-      return
-    }
+    // if loadImage() && !!!!!!
+    //  if (text.trim().length > 0){
+    //    alert("сука тока либо текст либо картинка");
+    //    document.getElementsByClassName("send_img")[0].value = "";
+    //    document.getElementsByClassName("chatbox")[0].value = "";
+    //    return
+    //  }
 
     var url = '/request';
 
     // create chatBubble for user, scroll and save to localStorage
-
-    if (loadImage()){
+    // if (loadImage()) !!!
+    if (false){
       var userBubbleId = makeUserBubble(text, 1);
       var waitForImage = new Promise (async (resolve, reject) => {
         try{
+        //imageHash = await getImageHash(loadImage());
           imageHash = await getImageHash(loadImage());
           editUserBubble(id=userBubbleId, {image: imageHash["imageName"]});
           saveToLocal("user", text, imageHash["imageName"]);
@@ -305,16 +226,13 @@ document.addEventListener('DOMContentLoaded', restoreHtmlFromLocalStorage);
     // remove text from textbox
     document.querySelector('.chatbox').value = "";
 
-    //var botBubbleId = makeBotBubble("") + 1;
-    var botBubbleId = makeBotBubble("") + 1;
-    var botBubbleID = document.getElementsByClassName("userInput").length + 1;
-    console.log(botBubbleID);
-    scrollToBottom();
+    var botBubbleId = makeBotBubble("");
 
     // get server response
     var promise = new Promise(async (resolve, reject) => {
         try {
-          result = await sendRequest(url, text, loadImage());
+          //result = await sendRequest(url, text, loadImage());!!!
+          result = await sendRequest(url, text);
           resolve(result);
         } catch (error) {
           reject(error);
@@ -323,16 +241,12 @@ document.addEventListener('DOMContentLoaded', restoreHtmlFromLocalStorage);
 
     promise.then((resp) => {
       // fill the bot bubble and scroll to bottom
-      replaceBotAnimwithBubble("");
-      document.querySelectorAll(".id-" + botBubbleID + ".bot__output")[0].innerHTML = resp["text"];
+      document.querySelectorAll(".id-" + botBubbleId + ".bot__output")[0].innerHTML = resp["text"];
       scrollToBottom();
       saveToLocal("bot", resp["text"], resp["image"]);
-      waitingForResponse = false;
 
     }).catch((error) => {
       console.error('Async function failed with error:', error);
-      waitingForResponse = false;
-
     });
   }
 
@@ -405,34 +319,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-
-const sendButton = document.getElementById('sendBtn');
-sendButton.addEventListener('click', function() {
-    if (waitingForResponse) {
-        return 0
-    } else {
-        makeBubbles();
-    }});
-
-textInput.addEventListener("keydown", function(event) {
-  if (event.key === "Enter" && !event.shiftKey) {
-    event.preventDefault();
-    if (waitingForResponse) {
-      return 0
-    } else {
-      makeBubbles();
-    }
-  }
-  if (event.key === "Enter" && event.shiftKey){
-    event.preventDefault();
-    const start = this.selectionStart;
-    const end = this.selectionEnd;
-    this.value = this.value.substring(0, start) + "\n" + this.value.substring(end);
-    this.selectionStart = this.selectionEnd = start + 1;
-  }
-});
-
- document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     var modal = document.getElementById("modal");
     var acceptBtn = document.getElementById("acceptBtn");
 
