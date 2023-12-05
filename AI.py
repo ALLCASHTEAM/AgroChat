8e6a304a00968d410ec18889b1a0efdc05e97aa8
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2023-12-05 23:29:42 +0300

    Очистил репозиторий (сорри)

--- AI.py
+++
@@ -1,3 +0,0 @@
-from deeppavlov import build_model
-model = build_model('qa_squad2_bert', download=True, install=True)
-model(['DeepPavlov is a library for NLP and dialog systems.'], ['What is DeepPavlov?'])
--- AI_PRO_MAX/classify_personal_questions.py
+++ AI_PRO_MAX/classify_personal_questions.py
@@ -1,12 +1,10 @@
 import joblib
 
-# Загрузка модели и векторизатора
 model = joblib.load('./AI_PRO_MAX/logistic_model_personal_questions.pkl')
 vectorizer = joblib.load('./AI_PRO_MAX/tfidf_vectorizer_personal_questions.pkl')
 
 
 def is_personal(question):
-    # Очистка и векторизация вопроса
     question_clean = question.replace('[^\w\s]', '').lower()
     question_vectorized = vectorizer.transform([question_clean])
     # Предсказание с помощью модели
