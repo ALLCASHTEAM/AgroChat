3bc21c03ddf4b82161e6066479041218d04d2480
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2024-02-06 18:16:23 +0300

    biba

--- AI_PRO_MAX/gen_engine.py
+++ AI_PRO_MAX/gen_engine.py
@@ -1,6 +1,6 @@
 from llama_cpp import Llama
 import torch
-
+max_tokens = 1999
 def interact():
     model = Llama(
         model_path='./AI_PRO_MAX/model-q16_0.gguf',
@@ -36,7 +36,24 @@ def generate(question, model, context=False):
                 }
             ]
         )
+    user_req = ["user", question, answer["promt_tokens"]]
+    assistant_req = ["assistant", answer["choices"][0]["message"]["content"], answer["completion_tokens"]]
+    print(user_req, '\n')
+    print(assistant_req, '\n')
+    dialog_history = []  # !!!#
+    # Указываем максимальное количество токенов на историю
+    # Дальнейший дрочreqs_sum = user_req[2] + assistant_req[2]
+    sum_third_column = sum(int(row[2]) for row in dialog_history if isinstance(row[2], (int, float)))
+    # Чистка истории диалога что бы новые смски вмещалисьwhile reqs_sum + sum_third_column >= max_tokens:
+    dialog_history.pop(0)
+    sum_third_column = sum(int(row[2]) for row in dialog_history if isinstance(row[2], (int, float)))
 
+    # добавление после всего говна в историю
+    dialog_history.append(user_req)
+    dialog_history.append(assistant_req)
+    #
+    # Сохранение истории диалога в локал стораге#!!!
+#
     return answer["choices"][0]["message"]["content"]
 
 
