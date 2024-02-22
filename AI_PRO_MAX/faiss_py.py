200cc31929c1799828af021f9175ed8c69aaa349
Azamatik159 <58002957+Azamat1k159@users.noreply.github.com>
2024-02-23 00:31:37 +0300

    Оптимизация ураа

--- AI_PRO_MAX/faiss_py.py
+++ AI_PRO_MAX/faiss_py.py
@@ -3,46 +3,40 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 from pathlib import Path
+
 model = SentenceTransformer('hivaze/ru-e5-large')
 
 
 def read_and_preprocess_data(folder_path):
-    questions_answers = []
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith(".txt"):
-            file_path = os.path.join(folder_path, file_name)
-            with open(file_path, 'r', encoding='utf-8') as file:
-                for line in file:
-                    parts = line.strip().split(" | ")
-                    if len(parts) == 2:
-                        questions_answers.append((parts[0], parts[1]))
-    return questions_answers
+    folder_path = Path(folder_path)
+    return [(parts[0], parts[1]) for file in folder_path.glob("*.txt") for parts in
+            [line.strip().split(" | ") for line in file.open('r', encoding='utf-8')] if len(parts) == 2]
 
 
 def vectorize_questions(questions):
-    return model.encode(questions)
+    # Добавьте здесь вашу логику предварительной обработки, если это необходимо
+    return model.encode(questions, batch_size=64)  # Предполагается, что модель поддерживает пакетную обработку
 
 
 def create_faiss_index(vectors):
-    dimension = vectors.shape[1]  # Получаем размерность векторов
-    index = faiss.IndexFlatL2(dimension)  # Создаем индекс для L2 расстояния
-    index.add(vectors)  # Добавляем векторы в индекс
+    dimension = vectors.shape[1]
+    index = faiss.IndexFlatL2(dimension)
+    index.add(vectors)
     return index
 
 
 def search(query, index, questions_answers, top_k=30):
-    query_vector = vectorize_questions([query])[0].astype('float32')  # Векторизуем запрос
-    distances, indices = index.search(np.array([query_vector]), top_k)  # Поиск в Faiss
-    answer = [[questions_answers[i][0], questions_answers[i][1]] for j, i in enumerate(indices[0])]
-    return answer
+    query_vector = vectorize_questions([query])[0].astype('float32')
+    distances, indices = index.search(np.array([query_vector]), top_k)
+    return [[questions_answers[i][0], questions_answers[i][1]] for i in indices[0]]
 
 
-def list_return(user_query) -> list: # Замените на интересующий вопрос
+def list_return(user_query) -> list:
     return search(user_query, index, questions_answers)
 
 
 folder_path = Path(__file__).resolve().parent.parent / "rofls"
 questions_answers = read_and_preprocess_data(folder_path)
-questions = [qa[1] for qa in questions_answers]  # Извлекаем вопросы
+questions = [qa[1] for qa in questions_answers]
 vectors = vectorize_questions(questions)
 index = create_faiss_index(np.array(vectors).astype('float32'))
